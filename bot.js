8b411791f9501a65de2718639b49548f4293f005
Arjun Krishna <arjun12345krishna@gmail.com>
2020-09-06 02:13:27 +0530

    Post scheduling:

--- bot.js
+++ bot.js
@@ -8,37 +8,49 @@ var T = new Twit(config);
 
 
 
-
 // Get Tweets/Search from Twitter
-
-// var params = {
-//     q: '#testmytwitterbot : since:2020-08-01',
-//     count: 10
-// }
-
-// function getData(err, data, response) {
-//     var tweets = data.statuses;
-//     for (var i = 0; i < tweets.length; i++) {
-//         console.log(tweets[i].text);
-//     }
-// }
-
-// T.get('search/tweets', params, getData);
-
-
-
-
-
-var params = {
-    status: '#testmytwitterbot from Decoded_Cipher'
-}
-
-function postData(err, data, response) {
-    if (err) {
-        console.log('Something went Wrong!')
-    } else {
-        console.log('It worked!');
+function getTweet() {
+    var params = {
+        q: '@MyCipherBot : since:2020-08-01',
+        count: 10
     }
+
+    function getData(err, data, response) {
+        if (err) {
+            console.log('getData() - Cannot fetch details, something went Wrong!')
+        } else {
+            var tweets = data.statuses;
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].text);
+            }
+        }
+    }
+
+    T.get('search/tweets', params, getData);
 }
 
-T.post('statuses/update', params, postData);
+// getTweet();
+
+
+// Post Tweets/Updates to Twitter
+function postTweet() {
+    var r = Math.floor(Math.random()*100);
+    var postParams = {
+        // status: 'This is just a test tweet from @MyCipherBot!'
+        status: 'Random Number: ' + r
+    }
+
+    function postData(err, data, response) {
+        if (err) {
+            console.log('postData() - Cannot post the tweet, something went Wrong!')
+        } else {
+            console.log('It worked!');
+        }
+    }
+
+    T.post('statuses/update', postParams, postData);
+}
+
+postTweet();
+setInterval(postTweet, 1000*20);
+

00365fa21456bdbf8d7bac00da769511665e1488
Arjun Krishna <arjun12345krishna@gmail.com>
2020-09-07 23:56:27 +0530

    Heroku Deploy with dotenv

--- bot.js
+++ bot.js
@@ -1,14 +1,17 @@
 // console.log('The Bot starts running...!');
+require('dotenv').config()
 var Twit = require('twit')
 
-var config = require('./config');
-// var config = {
-//     consumer_key: 'consumer_key',
-//     consumer_secret: 'consumer_secret',
-//     access_token: 'access_token',
-//     access_token_secret: 'access_token_secret',
-// }
+// var config = require('./config');
+
+var config = {
+    consumer_key: process.env.consumer_key,
+    consumer_secret: process.env.consumer_secret,
+    access_token: process.env.access_token,
+    access_token_secret: process.env.access_token_secret
+}
 // console.log(config);
+
 var T = new Twit(config);
 // console.log(T);
 
